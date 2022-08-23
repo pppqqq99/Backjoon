2cecffffed5fdb54cc01142f7148dd81c55e70ee
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2022-08-23 15:46:57 +0900

    [Gold V] Title: 공유기 설치, Time: 128 ms, Memory: 3572 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -1,49 +1,59 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+
 using namespace std;
 
-int n, c;
-long long home[200002];
+int n,c;
+vector <int> v;
 
-int decision(long long mid){
+// 가장 가까운 두 공유기 사이의 인접한 거리가 dis일 때 공유기를 c개 박을 수 있는지
+int decision(int dis){
+    int spot = v[0];
+    int sum=1;
 
-    long long spot = home[1];
-    int num=1;
-    for(int i=2;i<=n;i++){
-        
-        if(home[i]-spot >= mid){
-            num++;
-            spot = home[i];
+    for(int j=1; j<n; j++){
+        if(v[j] - spot >= dis){
+            spot = v[j];
+            sum++;
         }
     }
+
+    //cout<<dis<<", "<<sum<<endl;
     
-    return num >= c;
+    if(sum>=c) return true;
+    else return false;
+
+
 }
 
 int main(){
-    // 구간은 가장 짧은 interval
-    long long maxV=0, minV, ret, mid;
-    
     cin>>n>>c;
-    for(int i=1;i<=n;i++){
-        cin >> home[i]; 
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        v.push_back(a);
     }
-    sort(home+1, home+n+1);
-    minV=1;
-    maxV= home[n] - home[1];
-    
+    sort(v.begin(), v.end());
+
+    int minV = 1;
+    int maxV = v[n-1] - v[0];
+    int res=0;
     while(minV <= maxV){
-        mid =(minV+maxV)/2;
-        
+        int mid = (minV + maxV)/2; // 가장 인접한 두 공유기 사이의 거리를 mid라고 하자
+        // cout<<mid<<endl;
+
+
+        // 가장 인접한 두 공유기 사이의 거리가 mid라고 가정할 때 공유기를 c개 설치할 수 있는 mid값의 최대 찾기
+
+        // 공유기를 c개 이상 설치할 수 있다면 -> mid의 길이를 늘려서도 되는지 확인
         if(decision(mid)){
             minV = mid+1;
-            ret = mid;
-        }else{
+            res = mid;
+        }else{ // 공유기를 c개 미만으로 설치할 수 있다면 -> mid의 길이를 줄여서 c개 이상 설치 가능한 지 확인
             maxV = mid-1;
         }
     }
-    
-    cout<< ret;
-    
-    
+
+    cout<<res<<endl;
 }
