e1b1f35ac0b21fb89939b85e8cdfe08167482c15
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2023-01-20 20:33:34 +0900

    [Gold II] Title: 친구 네트워크, Time: 96 ms, Memory: 18916 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
@@ -1,94 +1,91 @@
 #include <iostream>
-#include <cstring>
-#include <vector>
-#include <queue>
 #include <algorithm>
+#include <vector>
+#include <cstring>
 #include <unordered_map>
 
 using namespace std;
+unordered_map <string, int> name;
+// vector <string> name;
+vector <int> parent, cnt;
 
-// 4195 - 친구 네트워크
-unordered_map<string, int> hashmap; // first: id, second: index
-vector<int> parent, friend_num; // disjoint-set을 위한 배열, 연결된 친구가 몇 명인지 저장하는 변수
-
-
-int getParent(int x) {
-    if (parent[x] == x) return x;
-    return parent[x] = getParent(parent[x]);
+int getParent(int a){
+    if(a == parent[a]) return a;
+    return parent[a] = getParent(parent[a]);
 }
 
-void unionParent(int a, int b) {
-    a = getParent(a);
-    b = getParent(b);
-    
-    // 수가 더 큰 쪽에 있는 친구의 수를, 수가 더 작은 쪽에 더한다
-    if (a < b) {
-        parent[b] = a;
-        friend_num[a] += friend_num[b];
-    }
-    else if(a>b){
-        parent[a] = b;
-        friend_num[b] += friend_num[a];
+void unionParent(int a, int b){
+    int p1 = getParent(a);
+    int p2 = getParent(b);
+
+
+    if(p1 < p2){
+        parent[p2] = p1;
+        cnt[p1] += cnt[p2];
+    }else if(p1 > p2){
+        parent[p1] = p2;
+        cnt[p2] += cnt[p1];
     }
 }
 
-void initParent(int num){
-    // 한 관계에 두 개의 id가 주어지기 때문에
-    // 최대 사람 수는 (관계의 수) * 2 이다.
-    parent.assign(num * 2 + 1, 0);
-    friend_num.assign(num * 2 + 1, 1);
+// int findParent(int a, int b){
+//     int p1 = getParent(a);
+//     int p2 = getParent(b);
 
-    for (int i = 0; i <= num * 2; i++) {
-        parent[i] = i;
-    }
-}
+//     if(p1 == p2) return 1;
+//     else return 0;
+// }
 
-int main() {
 
+
+int t,f;
+int main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int testcase;
+    cin>>t;
 
-    cin >> testcase;
-    while(testcase--){
-        hashmap.clear();
-        int idx = 0, f = 0;
+    while(t--){
+        cin>>f;
 
-        cin >> f;
-        initParent(f);
+        name.clear();
+        parent.assign(f*2+1, 0);
+        cnt.assign(f*2+1, 1);
 
-        // id_1의 인덱스 번호, id_2의 인덱스 번호
-        int friend_n1 = 0, friend_n2 = 0;
-
-        for (int i = 0; i < f; i++) {
-            string id_1, id_2;
-            cin >> id_1 >> id_2;
-
-            // key id_1에 해당하는 value의 개수가 없을 때
-            if (hashmap.count(id_1) == 0) {
-                hashmap[id_1] = ++idx;
-                friend_n1 = idx;
-            }
-            else { // 이미 존재한다면 key에 해당하는 value를 가지고 온다
-                friend_n1 = hashmap[id_1];
-            }
-
-            if (hashmap.count(id_2) == 0) {
-                hashmap[id_2] = ++idx;
-                friend_n2 = idx;
-            }
-            else {
-                friend_n2 = hashmap[id_2];
-            }
-
-            // unionParent를 통해 부모를 합친다.
-            unionParent(friend_n1, friend_n2);
-            int target = getParent(friend_n1);
-            cout << friend_num[target] << '\n';
+        for(int i=0; i<=f*2+1; i++){
+            parent[i] = i;
         }
-    }
 
-    return 0;
+        int idx=0;
+
+        for(int i=1; i<=f; i++){
+            string str1, str2;
+            cin>>str1>>str2;
+            
+            int f1 = 0, f2 = 0;
+            
+            // 문자열이 해쉬맵에 존재한다면 해당 key의 value값 리턴
+            //iter = name.find(str1);
+            if(name.count(str1) != 0){
+                f1 = name[str1];
+            }else{
+                name[str1] = ++idx;
+                f1 = idx;
+            }
+
+            // iter = name.find(str2);
+            if(name.count(str2) != 0){
+                f2 = name[str2];
+            }else{
+                name[str2] = ++idx;
+                f2 = idx;
+            }
+ 
+            unionParent(f1, f2);
+            int target = getParent(f1);
+            cout<<cnt[target]<<"\n";
+        }
+
+    }
 }
