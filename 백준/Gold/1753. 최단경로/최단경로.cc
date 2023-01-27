d40b40c10505171503051cdd11a9c80f2504e880
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2023-01-27 17:39:41 +0900

    [Gold IV] Title: 최단경로, Time: 936 ms, Memory: 9092 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -9,51 +9,67 @@ using namespace std;
 
 typedef pair<int, int> pii;
 vector <pii> v[MAX_N];
+priority_queue<pii, vector<pii>, greater<pii>>pq;
+bool visited[MAX_N] = {false};
 
 int n, e, s;
 int dist[MAX_N];
 
 void dijkstra(int src){
-    priority_queue<pii, vector<pii>, greater<pii>>pq;
-    bool visited[MAX_N] = {false};
 
+    // dist 배열 초기화
     for(int i=1; i<=n; i++){
         dist[i] = INF;
     } 
     dist[src] = 0;
+
+    // pq에 시작점 삽입
     pq.push({0, src});
 
+    // pq가 빌 때까지
     while(!pq.empty()){
+
+        // pq에 삽입된 노드 중 src와 가장 가까운 노드 뽑기
         int u = pq.top().second;
-        //if(u == dst) return pq.top().first;
         pq.pop();
+
+        // 방문한 적이 있는 노드라면 스킵 
         if(visited[u]) continue;
 
+        // 방문 체크 - u노드는 0-u 까지의 최단경로임이 보장됨
         visited[u] = true;
 
+        // dist 배열 갱신 및 pq 삽입
         for(int i=0; i<v[u].size(); i++){
-            if(dist[v[u][i].second] > dist[u] + v[u][i].first){
-                dist[v[u][i].second] = dist[u] + v[u][i].first;
-                pq.push({dist[v[u][i].second], v[u][i].second});
+            
+            int targetNode = v[u][i].first;
+            int cost = v[u][i].second;
+
+            if(dist[targetNode] > dist[u] + cost){
+                dist[targetNode] = dist[u] + cost;
+                pq.push({dist[targetNode], targetNode});
             }
         }
     }
-    // return INF;
 
 }
 
+
 int main(){
+
+    // 입력
     cin>>n>>e;
     cin>>s;
-    
     for(int i=0; i<e; i++){
         int start, end, value;
         scanf("%d %d %d", &start, &end, &value);
-
-        v[start].push_back({value, end});
+        v[start].push_back({end, value});
     }
 
+    // dijkstra 알고리즘 수행
     dijkstra(s);
+
+    // 출력
     for(int i=1; i<=n; i++){
         if(dist[i] == INF) cout<<"INF"<<endl;
         else cout<<dist[i]<<endl;
