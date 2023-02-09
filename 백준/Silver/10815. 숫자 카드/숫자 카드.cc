25e3477bc0e3ececa8b52c5c7f8fe26a1f0c734f
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2023-02-09 16:22:51 +0900

    [Silver V] Title: 숫자 카드, Time: 224 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <algorithm>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 int n,m;
 
 unordered_map <int, int> map;
+vector <int> v;
 
+int find_num(int t){
+    int l=0, r=n-1;
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(t == v[mid]){
+            return 1;
+        }
+
+        if(v[mid] < t){
+            l = mid+1;
+        }else if(v[mid] > t){
+            r = mid-1;
+        }
+    }
+
+    return 0;
+}
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -16,15 +35,13 @@ int main(){
     cin>>n;
     for(int i=0; i<n; i++){
         cin>>t;
-        map.insert({t, 1});
+        v.push_back(t);
     }
+    sort(v.begin(), v.end());
+
     cin>>m;
     for(int i=0; i<m; i++){
         cin>>t;
-        if(map.count(t) >= 1){
-            cout<<"1 ";
-        }else{
-            cout<<"0 ";
-        }
+        cout<<find_num(t)<<" ";
     }
 }
