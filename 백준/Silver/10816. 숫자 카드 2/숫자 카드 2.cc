c78e5a1162f8d37f2886c0a1877f907106ee2628
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2023-02-09 16:08:01 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 308 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -7,6 +7,7 @@ using namespace std;
 
 int n,m;
 
+vector <int> v;
 unordered_map <int, int> checked;
 
 
@@ -16,19 +17,26 @@ int main(){
     cout.tie(NULL);
     int t;
     cin>>n;
+    // for(int i=0; i<n; i++){
+    //     cin>>t;
+    //     if(checked.count(t) > 0){
+    //         checked[t]++;
+    //     }else{
+    //         checked.insert({t, 1});
+    //     }
+    // }
+
     for(int i=0; i<n; i++){
         cin>>t;
-        if(checked.count(t) > 0){
-            checked[t]++;
-        }else{
-            checked.insert({t, 1});
-        }
+        v.push_back(t);
     }
+    
+    sort(v.begin(), v.end());
 
     cin>>m;
     for(int i=0; i<m; i++){
         cin>>t;
-        cout<<checked[t]<<" ";
+        cout<<upper_bound(v.begin(), v.end(), t) - lower_bound(v.begin(), v.end(), t)<<" ";
     }
 
 }
