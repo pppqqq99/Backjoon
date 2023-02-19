b0378fee94a5cda974317b0097da22ad902dbc34
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2023-02-19 18:41:30 +0900

    [Gold V] Title: 탑, Time: 120 ms, Memory: 6300 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int n;
+typedef pair<int, int> pii;
+stack<pii> st;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>n;
+    int cnt=1;
+    while(n--){
+        int input, flag=0;
+        cin>>input;
+        
+        while(!st.empty()){
+            if(st.top().second > input){
+                cout<<st.top().first<<" ";
+                flag=1;
+                st.push({cnt, input});
+                break;
+            }else{
+                st.pop();
+            }
+        }
+
+        if(flag == 0){
+            st.push({cnt, input});
+            cout<<"0 ";
+        }
+
+        cnt++;
+    }
+}
