c4eae2273b28f324bc128c1d4666ff8ef24f49a9
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2023-02-23 21:08:32 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n%2){
+        cout<<"SK"<<endl;
+    }else{
+        cout<<"CY"<<endl;
+    }
+}
