19f414b153dd03daedbaeaf98514400f9fdb21e5
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2023-02-25 10:58:43 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
-SELECT animal_ins.name AS NAME, animal_ins.datetime AS DATETIME
-FROM Animal_ins LEFT OUTER JOIN Animal_outs ON animal_ins.animal_id = animal_outs.animal_id
-WHERE Animal_outs.animal_id IS NULL
-ORDER BY Animal_ins.datetime
+SELECT ai.NAME, ai.DATETIME
+FROM ANIMAL_INS ai LEFT OUTER JOIN ANIMAL_OUTS ao ON ai.animal_id = ao.animal_id
+WHERE ao.animal_id IS NULL
+ORDER BY ai.datetime
 LIMIT 3;
