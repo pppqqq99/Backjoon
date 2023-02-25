7871b96293eca777bf2412feb7e29d35832ed385
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2023-02-25 10:36:59 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,3 +1,3 @@
-SELECT ANIMAL_ID, NAME, IF(sex_upon_intake like 'Neutered%' or sex_upon_intake like 'Spayed%', 'O', 'X') AS '중성화'
+SELECT ANIMAL_ID, NAME, IF(SEX_UPON_INTAKE like 'Neutered%'or SEX_UPON_INTAKE like 'Spayed%', 'O', 'X') AS 중성화
 FROM ANIMAL_INS
-ORDER BY animal_id;
+ORDER BY ANIMAL_ID;
