e3f7ff1ca3f08080509b525dd5d4137cd19e4c35
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2023-02-25 11:13:03 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT Animal_outs.animal_id AS ANIMAL_ID, Animal_outs.name AS NAME
-FROM Animal_outs LEFT OUTER JOIN Animal_ins ON Animal_outs.animal_id = Animal_ins.animal_id
-WHERE Animal_ins.animal_id IS NULL
-ORDER BY Animal_outs.animal_id, Animal_outs.name;
+SELECT ao.ANIMAL_ID, ao.NAME
+FROM ANIMAL_OUTS ao LEFT OUTER JOIN ANIMAL_INS ai ON ao.animal_id = ai.animal_id
+WHERE ai.animal_id IS NULL
+ORDER BY ai.animal_id;
