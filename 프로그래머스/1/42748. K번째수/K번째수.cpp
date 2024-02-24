612a78f9d7eb38686dc2fa607b040a407746b9a7
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2024-02-24 18:57:03 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -8,25 +8,20 @@ using namespace std;
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     
-    // 배열을 잘라서 정렬할 수 있는가?
-    // commands 배열을 반복하며 i,j,k를 분리
+    
     
     for(int i=0;i<commands.size();i++){
-        int startIndex, endIndex, target;
-        startIndex = commands[i][0] - 1;
-        endIndex = commands[i][1];
-        target = commands[i][2] - 1;
         
-        vector <int> v;
-        // 벡터 부분 복사
-        copy(array.begin() + startIndex, array.begin() + endIndex, back_inserter(v));
+        int st = commands[i][0] - 1;
+        int en = commands[i][1];
+        int ta = commands[i][2] - 1;
+        vector <int> temp;
+        temp = array;
         
-        // 정렬
-        sort(v.begin(), v.end());
-        
-        //cout<<v.size()<<endl;
-        // // target을 answer에 저장
-        answer.push_back(v[target]);
+        //cout<<temp.size()<<endl;
+        // cout<<st<<", "<<en<<", "<<ta<<" /"<<endl;
+        sort(temp.begin() + st, temp.begin() + en);
+        answer.push_back(temp[st+ta]);
     }
     return answer;
 }
