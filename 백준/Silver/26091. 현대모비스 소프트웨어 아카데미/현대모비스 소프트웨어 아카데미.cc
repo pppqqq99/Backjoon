99fbcfc77a7abf2dddf797fc811f4b98fa85aa21
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2024-03-27 00:14:53 +0900

    [Silver I] Title: 현대모비스 소프트웨어 아카데미, Time: 72 ms, Memory: 3568 KB -BaekjoonHub

---
+++ 백준/Silver/26091. 현대모비스 소프트웨어 아카데미/현대모비스 소프트웨어 아카데미.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+vector <long long> v;
+
+
+int main(){
+    int n, cnt=0;
+    long long m;
+
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++){
+        int tmp;
+        cin>>tmp;
+        v.push_back(tmp);
+    }
+
+    sort(v.begin(), v.end());
+
+    int start=0, end=n-1;
+
+    while(start<end){
+
+        // 각 커서가 가리키는 수의 합이 m을 넘을 시 cnt++
+        if(v[start] + v[end] >= m){
+            start++;
+            end--;
+            cnt++;
+        }else{
+            start++;
+        }
+    }
+
+    cout<<cnt<<endl;
+
+
+}
