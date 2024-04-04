a5e653b9400b5191e8ac8aaba3ec850a97b4b0fc
임형준 <64363668+yimhyeongjun@users.noreply.github.com>
2024-04-04 19:47:45 +0900

    [Silver II] Title: DNA 비밀번호, Time: 12 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
@@ -5,6 +5,23 @@
 
 using namespace std;
 
+// 문자를 받아 문자에 해당하는 인덱스 리턴하는 함수
+int checkCh(char ch){
+    if(ch == 'A') return 0;
+    else if(ch == 'C') return 1;
+    else if(ch == 'G') return 2;
+    else return 3;
+}
+
+// 두 배열을 인자로 받아 두 배열의 값이 일치하는지 확인하는 함수
+bool checkPass(int* passArr, int* checkArr){
+    for(int i=0; i<4; i++){
+        if(passArr[i] < checkArr[i]) return false;
+    }
+    return true;
+}
+
+
 int main(){
 
     ios::sync_with_stdio(false);
@@ -12,50 +29,38 @@ int main(){
     cout.tie(NULL);
 
     int s, p, cnt=0;
-    int a=0, c=0, g=0, t=0;
     string str;
-    int a_v, c_v, g_v, t_v;
-
+    int checkArr[4] = {0,0,0,0};
+    int passArr[4] = {0,0,0,0};
 
     // 0. 입력
     cin>>s>>p;
     cin>>str;
-    cin>>a_v>>c_v>>g_v>>t_v;
-
-
-    // 1. 부분 문자열 초기 세팅
-    for(int i=0;i<p;i++){
-        if(str[i] == 'A') a++;
-        else if(str[i] == 'C') c++;
-        else if(str[i] == 'G') g++;
-        else if(str[i] == 'T') t++;
+    for(int i=0; i<4; i++){
+        cin>>checkArr[i];
     }
 
 
-    int start=0;
-    // 2. 부분 문자열 하나씩 이동하며 체크
-    for(int i=p;i<s+1;i++){
-        if(a >= a_v && c >= c_v && g >= g_v && t >= t_v){
-            cnt++;
-        }
-
-        if(i == s){
-            // cout<<start<<", "<<i<<endl;
-            break;
-        }
-
-        if(str[start] == 'A') a--;
-        else if(str[start] == 'G') g--;
-        else if(str[start] == 'C') c--;
-        else if(str[start] == 'T') t--;
-        else ;
-        start++;
-
-        if(str[i] == 'A') a++;
-        else if(str[i] == 'G') g++;
-        else if(str[i] == 'C') c++;
-        else if(str[i] == 'T') t++;
+    // 1. 첫번째 비밀번호 세팅, 비밀번호 조건 확인하여 cnt에 반영
+    for(int i=0; i<p; i++){
+        // str[i]의 비밀번호 추가
+        passArr[checkCh(str[i])]++;
     }
-    
+
+
+    // 2. 슬라이딩 알고리즘을 사용하여 비밀번호 조건 확인하며 cnt에 반영
+    for(int i=0; i+p-1<s; i++){
+
+
+        // 2-1. passArr와 checkArr의 개수가 같을 시 true 리턴
+        if(checkPass(passArr, checkArr)){ cnt++; } 
+        
+        // 2-2. start 커서 옮기며 passArr에 반영
+        passArr[checkCh(str[i])]--;
+
+        // 2-3. end 커서 옮기며 passArr에 반영
+        passArr[checkCh(str[i+p])]++;
+    }
+
     cout<<cnt<<endl;
 }
